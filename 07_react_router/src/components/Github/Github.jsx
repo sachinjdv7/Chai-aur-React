09b0245313b97786f8acaf32359620ce8d05f060
sachin jadhav <sachin.jadhav@tridiagonal.com>
2024-04-13 21:19:15 +0530

    complete react router crash course

---
+++ 07_react_router/src/components/Github/Github.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+function Github() {
+  const data = useLoaderData([]);
+  //   console.log(data);
+  //   useEffect(() => {
+  //     fetch("https://api.github.com/users/sachinjdv7")
+  //       .then((response) => response.json())
+  //       .then((data) => setData(data));
+  //   }, []);
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      Github followers: {data.followers}
+      <img src={data.avatar_url} alt="avatar" />
+    </div>
+  );
+}
+
+export default Github;
+
+export const githubInfoLoader = async () => {
+  const data = await fetch("https://api.github.com/users/sachinjdv7");
+  return data.json();
+};
---
+++ 07_react_router/src/components/index.js
@@ -0,0 +1,21 @@
+import Header from "./Header/Header";
+import Home from "./Home/Home";
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import User from "./User/User";
+import Github from "./Github/Github";
+import Layout from "../Layout";
+import Footer from "./Footer/Footer";
+import { githubInfoLoader } from "./Github/Github";
+
+export {
+  Header,
+  Home,
+  About,
+  Contact,
+  User,
+  Github,
+  Layout,
+  Footer,
+  githubInfoLoader,
+};
