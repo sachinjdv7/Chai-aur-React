bb62b9c40c1b724cf1fbdf424767c053d851b0e6
sachin jadhav <sachin.jadhav@tridiagonal.com>
2024-04-18 23:43:13 +0530

    redux toolkit done

---
+++ 11_Reudx_Toolkit_TodoAPP/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AddTodo from "./components/AddTodo";
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <>
+      <h1>Hello Brother</h1>
+      <AddTodo />
+      <Todo />
+    </>
+  );
+}
+
+export default App;
