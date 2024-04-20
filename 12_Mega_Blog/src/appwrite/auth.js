a8f196322bbee2c3138293e222953a99dc98af0c
sachin jadhav <sachin.jadhav@tridiagonal.com>
2024-04-20 20:52:49 +0530

    appwrite auth service created

---
+++ 12_Mega_Blog/src/appwrite/auth.js
@@ -0,0 +1,57 @@
+import { Client, Account, ID } from "appwrite";
+import config from "../confing/config";
+
+class AuthService {
+  client = new Client();
+  account;
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccout = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccout) {
+        // call another method
+        return this.login({ email, password });
+      } else {
+        return userAccout;
+      }
+    } catch (error) {
+      console.log("Appwrite service :: createAccount :: error", error);
+    }
+  }
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      console.log("Appwrite service :: login :: error", error);
+    }
+  }
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser :: error", error);
+    }
+    return null;
+  }
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite service :: logout :: error", error);
+    }
+  }
+}
+
+const authServie = new AuthService();
+
+export default authServie;
