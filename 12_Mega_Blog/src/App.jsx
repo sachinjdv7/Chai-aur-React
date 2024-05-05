59d18e4a326e15856535207bcffa3fb343e0ae72
sachinjadhav <sachinjdv7@gmail.com>
2024-05-05 23:32:53 +0530

    redux store get ready

--- 12_Mega_Blog/src/App.jsx
+++ 12_Mega_Blog/src/App.jsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authServie from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
 import "./App.css";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>A Blog with Appwrite</h1>
-    </>
-  );
+  const [loding, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authServie
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loding ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-100">
+      <div className="w-full block">
+        <Header />
+        <main>{/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
