57243338f8ff677dd5df1b1d7f2a5138733d1e36
sachinjadhav <sachinjdv7@gmail.com>
2024-05-10 23:23:58 +0530

    fix: logout button fix

--- 12_Mega_Blog/src/components/Header/LogoutBtn.jsx
+++ 12_Mega_Blog/src/components/Header/LogoutBtn.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import authServie from "../../appwrite/auth";
+import authService from "../../appwrite/auth";
 import { logout } from "../../store/authSlice";
 
 function LogoutBtn() {
   const dispatch = useDispatch();
-  const handleLogout = () => {
-    authServie.logout().then(() => {
+  const logoutHandler = () => {
+    authService.logout().then(() => {
       dispatch(logout());
     });
   };
   return (
     <button
-      className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
-      onClick={handleLogout}
+      className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+      onClick={logoutHandler}
     >
       Logout
     </button>
