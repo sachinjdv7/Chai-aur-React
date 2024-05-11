2fb85e1a8e5dc4eeb20b7d24118a3ed35a7b7f35
sachinjadhav <sachinjdv7@gmail.com>
2024-05-11 15:39:23 +0530

    fix: AllPosts component

--- 12_Mega_Blog/src/pages/AllPosts.jsx
+++ 12_Mega_Blog/src/pages/AllPosts.jsx
@@ -5,7 +5,7 @@ import appwriteService from "../appwrite/configuration";
 function AllPosts() {
   const [posts, setPosts] = useState([]);
   useEffect(() => {}, []);
-  appwriteService.getPosts([]).then(() => {
+  appwriteService.getPosts([]).then((posts) => {
     if (posts) {
       setPosts(posts.documents);
     }
@@ -16,7 +16,7 @@ function AllPosts() {
         <div className="flex flex-wrap">
           {posts.map((post) => (
             <div key={post.$id} className="p-2 w-1/4">
-              <PostCard post={post} />
+              <PostCard {...post} />
             </div>
           ))}
         </div>
